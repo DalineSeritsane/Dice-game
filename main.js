140749f46088f3d4a74ba23cd35a4fbf51b0c533
DalineSeritsane <dalinelerato@gmail.com>
2024-06-14 15:41:26 +0200

    update dice

---
+++ main.js
@@ -0,0 +1,32 @@
+let player1 = "Player1";
+let player2 = "Player2";
+
+function editNames(){
+    player1 = prompt("Change Player1 Name")
+    player2 = prompt("Change Player2 Name")
+
+    if(player1.length<1 || player2.lenght <1) {
+        alert("Please enter valid names");
+        return;
+    }
+    document.querySelector("p.Player1")
+    .innerHTML = player1;
+
+    document.querySelector("p.Player2")
+    .innerHTML = player2;
+
+}
+
+function rollTheDice() {
+    let diceNum1 = document.querySelector("img1");
+    let diceNum2 = document.querySelector("img2");
+
+    diceNum1.setAttribute("src", "dice-roll-dice-gif.gif")
+    diceNum2.setAttribute("src", "dice-roll-dice-gif.gif")
+
+    let result = document.querySelector('h1')
+    setTimeout(() => {
+        let randomNumber1 = Math.floor(Math.random()*6)+1;
+        let randomNumber2 = Math.floor(Math.random()*6)+1;
+    }, timeout)
+}
